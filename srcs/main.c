970c4c10d73470c2512821465f5eb90dec7b7c14
hubourge <hulo1920@gmail.com>
2024-12-26 23:04:59 +0100

    Handle simple alloc in tiny and small heap     - Create a heap, a block, and a chunk

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hubourge <hubourge@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 18:26:59 by hubourge          #+#    #+#             */
-/*   Updated: 2024/12/18 16:10:38 by hubourge         ###   ########.fr       */
+/*   Updated: 2024/12/26 22:43:41 by hubourge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,42 @@
 
 int main()
 {
-    printf("ft_strlen('malloc') = %d\n", ft_strlen("malloc"));
+    char *str = (char *)malloc(434);
+    char *str1 = (char *)malloc(10000);
+    int *str2 = (int *)malloc(1000);
+    int *str3 = (int *)malloc(304);
+
+    (void)str;
+    (void)str1;
+    (void)str2;
+    (void)str3;
+
+    printf("\n==================== MAIN ====================\n\n");
+
+    printf("str  = %p, %s\n", str, str);
+    printf("str1 = %p, %s\n", str1, str1);
+    printf("str2 = %p\n", str2);
+    printf("str3 = %p\n", str3);
+
+
+    // struct rlimit limit;
+    // if (getrlimit(RLIMIT_AS, &limit) == -1)
+    //     return (1);
+    // printf("PAGESIZE = %d\n", PAGESIZE);
+    // printf("TINY_S = %d\n", TINY_S);
+    // printf("SMALL_S = %d\n", SMALL_S);
+    // printf("TINY_S_MAX_ALLOC = %zu\n", TINY_S_MAX_ALLOC);
+    // printf("SMALL_S_MAX_ALLOC = %zu\n", SMALL_S_MAX_ALLOC);
+    printf("ALIGNED_DATA = %zu\n", ALIGNED_DATA);
+    printf("ALIGNED_HEAP = %zu\n", ALIGNED_HEAP);
+    printf("ALIGNED_LARGE_HEAP = %zu\n", ALIGNED_LARGE_HEAP);
+    printf("ALIGNED_BLOCK = %zu\n", ALIGNED_BLOCK);
+    printf("ALIGNED_CHUNK = %zu\n", ALIGNED_CHUNK);
+    
+    printf("\n===================== SHOW ===================\n\n");
+    show_alloc_debug();
+    printf("\n==================== DEBUG ===================\n\n");
+    show_alloc_mem();
+
+    printf("diff addr = %lu\n", (void*)0xA00BE - (void*)0xA006A);
 }
---
+++ srcs/malloc/malloc.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   malloc.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hubourge <hubourge@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/18 13:40:16 by hubourge          #+#    #+#             */
+/*   Updated: 2024/12/26 22:41:07 by hubourge         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "malloc.h"
+
+t_data *g_data = NULL;
+
+void *malloc(size_t size)
+{
+    printf("-> Size = %zu (%lu)bytes\n", size, sizeof(size)); /// debug
+    if (size == 0)
+        return (NULL);
+
+    if (g_data == NULL)
+    {
+        if (!data_init(&g_data))
+            return (NULL);
+    }
+
+    if (size <= TINY_S_MAX_ALLOC)
+        heap_alloc(g_data->tiny_heap, TINY_S, size);
+    else if (size <= SMALL_S_MAX_ALLOC)
+        heap_alloc(g_data->small_heap, SMALL_S, size);
+    // else
+    //     allocate_large(&g_data->large_heap, size);
+
+    printf("\n");
+    if (g_data->failed == false)
+        return (g_data->addr_return);
+    
+    return (NULL);
+}
+
+void *align(void *ptr_to_align)
+{
+    uintptr_t new_ptr = (uintptr_t)ptr_to_align;
+
+    if ((new_ptr % ALIGNMENT) != 0)
+        new_ptr += ALIGNMENT - (new_ptr % ALIGNMENT);
+
+    return ((void *)new_ptr);
+}
+
+void heap_alloc(t_heap *heap, size_t heap_pagesize, size_t size)
+{
+    t_block *block_tmp = NULL;
+    t_block *block = heap->first_block;
+
+    if ((size_t)heap_pagesize == (size_t)TINY_S)
+        printf("-> TINY HEAP\n"); /// debug
+    else
+        printf("-> SMALL HEAP\n"); /// debug
+        
+    while (block != NULL)
+    {
+        block_tmp = block;
+        block = block->next;
+    }
+
+    block = (t_block *)mmap(NULL, heap_pagesize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (block == MAP_FAILED)
+    {
+        g_data->failed = true;        
+        return ;
+    }
+
+    if (block_tmp != NULL)
+        block_tmp->next = block;
+    else
+        heap->first_block = block;
+    block->next = NULL;
+    block->free_size = heap_pagesize - ALIGNED_BLOCK;
+
+    chunk_alloc(block, size);
+}
+
+void    chunk_alloc(t_block *block, size_t size)
+{
+    block->first_chunk = (t_chunk *)((void *)block + ALIGNED_BLOCK);
+    
+    block->first_chunk->chunk = (t_chunk *)((void *)block->first_chunk + ALIGNED_CHUNK);
+    block->first_chunk->prev = NULL;
+    block->first_chunk->next = NULL;
+    
+    block->first_chunk->size = size;
+    printf("1meta + size = %lu\n", (size_t)align((void *)block + ALIGNED_BLOCK + ALIGNED_CHUNK + size) - (size_t)block - ALIGNED_BLOCK);
+    printf("2meta + size = %lu\n", (size_t)align((void *)block->first_chunk->chunk + size) - (size_t)block - ALIGNED_BLOCK);
+    printf("3meta + size = %lu\n", (size_t)align((void *)ALIGNED_CHUNK + size));
+    
+    block->free_size -= (size_t)align((void *)ALIGNED_CHUNK + size);
+    
+    // block->first_chunk->size_free = size;
+}
