791574f494442a78ac404c646a9d4adaef6c678c
hubourge <hulo1920@gmail.com>
2024-12-26 23:06:18 +0100

    Initialise data struct

---
+++ srcs/malloc/init.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hubourge <hubourge@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/21 17:42:39 by hubourge          #+#    #+#             */
+/*   Updated: 2024/12/25 19:38:04 by hubourge         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "malloc.h"
+
+int data_init(t_data **data)
+{
+    struct rlimit limit;
+    
+    if (getrlimit(RLIMIT_AS, &limit) == -1)
+        return (false);
+    if (ALIGNED_DATA + ALIGNED_HEAP * 2 >= limit.rlim_max)
+		return (false);
+
+    (*data) = (t_data *)mmap(NULL, ALIGNED_DATA + ALIGNED_HEAP * 2, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if ((*data) == MAP_FAILED)
+        return (false);
+
+    tiny_init(data);
+    small_init(data);
+    (*data)->large_heap = NULL;
+
+    (*data)->failed = false;
+    (*data)->addr_return = NULL;
+
+    return (true);
+}
+
+void    tiny_init(t_data **data)
+{
+    (*data)->tiny_heap = (t_heap *)((void *)(*data) + ALIGNED_DATA);
+    (*data)->tiny_heap->pagesize = TINY_S;
+    (*data)->tiny_heap->first_block = NULL;
+}
+
+void    small_init(t_data **data)
+{
+    (*data)->small_heap = (t_heap *)((void *)(*data)->tiny_heap + ALIGNED_HEAP);
+    (*data)->small_heap->pagesize = SMALL_S;
+    (*data)->small_heap->first_block = NULL;
+}
